8c26412dfdc63bbf8e84551c17593f6b8acb5506
wilbrone <wilbroneokoth@gmail.com>
2020-02-21 13:02:59 +0300

    add urls for search and adding new neighborhood

--- hood/urls.py
+++ hood/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('search/', views.search_hoods, name='search'),
+    path('addneighborhood',views.addNeighborhood,name="addNeighborhood"),
+    
 ]
